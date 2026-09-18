bbee7e5052e3d8db5898a0a1cb698609e9b6bd97
Sujan Billava <billavasujan@gmail.com>
2026-09-18 10:42:31 +0530

    Day-69 ThreadName

---
+++ Day-69/ThreadName.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class MyThread extends Thread
+{
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+public class ThreadName 
+{
+    public static void main(String[] args) 
+    {
+        MyThread t=new MyThread();
+        //t.setName("DownloadThread");
+        t.start();    
+    }    
+}
